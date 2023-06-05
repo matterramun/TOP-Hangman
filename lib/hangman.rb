17b41900b731fd29f25d6b6aa30bd56d1ea66e48
matterramun <Veradux4@protonmail.com>
2023-06-05 13:28:55 -0400

    Add all code to a class for improving var scope.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,64 +2,79 @@
 
 require 'cli/ui'
 
-def loader
-  puts 'Loading...'
-end
-
-def new_game
-  word = generate_word
-  word_array = word.chars
-  guess_array = Array.new(word_array.length) { |i| i = '_' }
-  puts "#{word} #{word_array.length} #{guess_array}"
-end
-
-def saver; end
-
-def generate_word
-  word = ''
-  loop do
-    word = File.new('word_list.txt').readlines[Random.new.rand(0..10_000)].to_s.chomp
-    next unless word.length.between?(5, 12)
-
-    break
+class Hangman
+  def loader
+    puts 'Loading...'
   end
-  word
-end
 
-def play_round
-  loop do
-    CLI::UI::Prompt.ask('Play or Save?') do |handler|
-      handler.option('Play') { guess }
-      handler.option('Save') { saver }
+  def new_game
+    word = generate_word
+    word_array = word.chars
+    @guess_array = Array.new(word_array.length) { '_' }
+    puts "#{word} #{word_array.length} #{@guess_array}"
+  end
+
+  def saver; end
+
+  def generate_word
+    word = ''
+    loop do
+      word = File.new('word_list.txt').readlines[Random.new.rand(0..10_000)].to_s.chomp
+      next unless word.length.between?(5, 12)
+
+      break
     end
-    exit if victory_check
+    word
   end
-end
 
-def guess
-  loop do
-    guess_attempt = CLI::UI.ask('Guess a letter!').downcase
-    next unless guess_attempt.match?(/[a-z]/)
-
-    return
-  end
-  guess_attempt
-end
-
-def victory_check
-  false
-end
-
-CLI::UI::StdoutRouter.enable
-CLI::UI::Prompt.ask('New Game or Load Game?') do |handler|
-  handler.option('New Game')  { new_game }
-  handler.option('Load Game') do
-    if File.exist?('savefile.txt')
-      loader
-    else
-      puts 'Save file does not exist!'
-      exit
+  def play_round
+    loop do
+      CLI::UI::Prompt.ask('Play or Save?') do |handler|
+        handler.option('Play') { guess }
+        handler.option('Save') { saver }
+      end
+      exit if victory_check
     end
   end
-  play_round
+
+  def guess
+    puts @guess_array.join(' ')
+    loop do
+      guess_attempt = CLI::UI.ask('Guess a letter!').downcase
+      next unless guess_attempt.match?(/[a-z]/)
+
+      return
+    end
+    guess_attempt
+  end
+
+  def guess_attempt
+    match_array = word_array.select { |letter| letter == guess_attempt }
+    match_array.each do |index|
+      @guess_array[index] = guess_attempt
+    end
+  end
+
+  def victory_check
+    false
+  end
+
+  def initialize
+    @guess_array = []
+    CLI::UI::StdoutRouter.enable
+    CLI::UI::Prompt.ask('New Game or Load Game?') do |handler|
+      handler.option('New Game')  { new_game }
+      handler.option('Load Game') do
+        if File.exist?('savefile.txt')
+          loader
+        else
+          puts 'Save file does not exist!'
+          exit
+        end
+      end
+    end
+    play_round
+  end
 end
+
+Hangman.new
